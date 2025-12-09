dcff3db53b82996d6eeb6c811cdda1d6289fb28b
Qiyue Chen <qchen438@gatech.edu>
2025-12-09 00:46:37 -0500

    Remove build artifacts and generated experiment files from git tracking

--- build/CMakeFiles/ssd-fairness.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ssd-fairness.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hice1/qchen438/Documents/ssd-fairness
-# Build directory: /home/hice1/qchen438/Documents/ssd-fairness/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ssd-fairness-tests "/home/hice1/qchen438/Documents/ssd-fairness/build/ssd-fairness-tests")
-set_tests_properties(ssd-fairness-tests PROPERTIES  _BACKTRACE_TRIPLES "/home/hice1/qchen438/Documents/ssd-fairness/CMakeLists.txt;42;add_test;/home/hice1/qchen438/Documents/ssd-fairness/CMakeLists.txt;0;")
